188af9976f98bceb443ecb4aae973775e818dd06
Pablo <pablo22php@gmail.com>
2019-01-08 16:47:06 +0100

    Travis2

---
+++ TAP-master/vaadinpokedex-tap-master/src/main/java/pamigui/vaadin_inventario/MetodoVisualizar.java
@@ -0,0 +1,11 @@
+package pamigui.vaadin_inventario;
+
+import java.util.ArrayList;
+import java.util.List;
+
+
+public interface MetodoVisualizar {
+	public void visualizar(List<Producto> inventario);
+
+
+}
